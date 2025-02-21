9f6e60e30c756c3978ec97af602b7885c8f793d4
wevemedeiros <wguedes@refmais.com.br>
2025-02-21 11:53:48 -0300

    fix

--- components/utility/global-state.tsx
+++ components/utility/global-state.tsx
@@ -116,7 +116,7 @@ export const GlobalState: FC<GlobalStateProps> = ({ children }) => {
   const [userInput, setUserInput] = useState<string>("")
   const [chatMessages, setChatMessages] = useState<ChatMessage[]>([])
   const [chatSettings, setChatSettings] = useState<ChatSettings>({
-    model: "gpt-4o",
+    model: "openai/gpt-4o",
     prompt: "You are a helpful AI assistant.",
     temperature: 0.5,
     contextLength: 4000,
